239077ba167dad16c1ed01063ccd1a20dd95bf3a
Pavan <ashithilakarathna@gmail.com>
2025-07-17 13:59:11 +0530

    Successfully create getProductById function and add the getProductById route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { getProducts,saveProduct,deleteProduct, updateProduct } from "../controllers/productController.js";
+import { getProducts,saveProduct,deleteProduct, updateProduct , getProductById } from "../controllers/productController.js";
+import { get } from "mongoose";
 
 const productRouter = express.Router();     //create a router
 
@@ -10,5 +11,7 @@ productRouter.post("/",saveProduct);        //save a product
 productRouter.delete("/:productId", deleteProduct);        //delete a product
 
 productRouter.put("/:productId", updateProduct);        //update a product
+
+productRouter.get("/:productId", getProductById);        //get a product
 
 export default productRouter; //export the router to use in index
